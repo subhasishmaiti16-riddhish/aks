d6927f4633bfc7d08a5fc09622df7273ec23b797
Subhasish Maiti <subhasishmaiti16@gmail.com>
2026-08-16 16:15:13 +0530

    subnet module add

--- parent/main.tf
+++ parent/main.tf
@@ -11,8 +11,8 @@ module "vnet-parent" {
 }
 
 module "subnet-parent" {
-  depends_on = [ module.vnet-parent ]
-  source = "../child/subnet"
+  depends_on   = [module.vnet-parent]
+  source       = "../child/subnet"
   subnet-child = var.subnet-parent
-  
+
 }
